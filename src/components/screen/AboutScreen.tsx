c08722e4086e9d0e2be9a157619614306cd601ec
mon <mon.dev005@gmail.com>
2026-03-27 00:41:37 +0800

    finished the entrie about screen component.

--- src/components/screen/AboutScreen.tsx
+++ src/components/screen/AboutScreen.tsx
@@ -1,9 +1,15 @@
+import { Section1 } from "../../sections/about/section1";
+import { Section2 } from "../../sections/about/section2";
+import { Section3 } from "../../sections/about/section3";
+import { Section4 } from "../../sections/about/section4";
+
 export function AboutScreen() {
   return (
-    <>
-      <h1 className="flex items-center justify-center min-h-screen text-3xl">
-        This is About
-      </h1>
-    </>
+    <main className="min-h-screen bg-base flex flex-col pt-20">
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+    </main>
   );
 }
