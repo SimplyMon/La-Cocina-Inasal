f79646a363bcd0c2a241fa18ae9f2d5651ba255e
mon <mon.dev005@gmail.com>
2026-03-26 23:03:13 +0800

    added home screen about, and menu screen and added scroll to top.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { Route, Routes } from "react-router-dom";
-import { HomeScreen } from "./components/screen/HomeScreen";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { HeaderComponent } from "./components/layout/HeaderComponent";
 import { FooterComponent } from "./components/layout/FooterComponent";
-import { AboutScreen } from "./components/screen/AboutScreen";
 import { NotFound } from "./components/layout/NotFound";
+import { HomeScreen } from "./components/screen/HomeScreen";
+import { MenuScreen } from "./components/screen/MenuScreen";
+import { AboutScreen } from "./components/screen/AboutScreen";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <HeaderComponent />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/menu" element={<MenuScreen />} />
         <Route path="/about" element={<AboutScreen />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
