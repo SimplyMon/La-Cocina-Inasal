d3b1d901485ae5c978cd963197132324b7db64f2
mon <mon.dev005@gmail.com>
2026-03-26 23:42:20 +0800

    finished the entrie homescreen component.

--- src/components/screen/HomeScreen.tsx
+++ src/components/screen/HomeScreen.tsx
@@ -1,9 +1,21 @@
+import { Section1 } from "../../sections/home/section1";
+import { Section2 } from "../../sections/home/section2";
+import { Section3 } from "../../sections/home/section3";
+import { Section4 } from "../../sections/home/section4";
+import { Section5 } from "../../sections/home/section5";
+import { Section6 } from "../../sections/home/section6";
+import { Section7 } from "../../sections/home/section7";
+
 export function HomeScreen() {
   return (
-    <>
-      <h1 className="flex items-center justify-center min-h-screen text-3xl">
-        This is Home
-      </h1>
-    </>
+    <main className="min-h-screen bg-base flex flex-col">
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+    </main>
   );
 }
